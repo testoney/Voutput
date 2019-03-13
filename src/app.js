41632f4581ff817adb29790afabe69f1f2127c8f
oney <appdevoney@gmail.com>
2019-03-13 15:34:15 +0800

    remove some

--- src/app.js
+++ src/app.js
@@ -1,23 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-require('./db');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// require('./db');
 
-const app = express();
-const apolloServer = require('./graphql/apolloServer');
+// const app = express();
+// const apolloServer = require('./graphql/apolloServer');
 
-app.set('trust proxy', true);
-app.use(express.static('public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.set('trust proxy', true);
+// app.use(express.static('public'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-apolloServer.applyMiddleware({ app });
+// apolloServer.applyMiddleware({ app });
 
-app.get('/hello', (req, res) => {
-  res.json({ hello: 'word' });
-});
+// app.get('/hello', (req, res) => {
+//   res.json({ hello: 'word' });
+// });
 
-app.listen(3009, () => {
-  console.log('app listen on port: 3009');
-});
+// app.listen(3009, () => {
+//   console.log('app listen on port: 3009');
+// });
 
-module.exports = app;
+// module.exports = app;
+
+const schema = require('./graphql/schema');
--- src/graphql/apolloServer.js
+++ src/graphql/apolloServer.js
@@ -1,23 +1,23 @@
-const schema = require('./schema');
-const { ApolloServer } = require('apollo-server-express');
+// const schema = require('./schema');
+// const { ApolloServer } = require('apollo-server-express');
 
-const genLoaders = require('./genLoaders');
+// const genLoaders = require('./genLoaders');
 
-const server = new ApolloServer({
-  schema,
-  context: async () => {
-    const context = {
-      loaders: genLoaders(),
-    };
-    return context;
-  },
-  introspection: true,
-  playground: {
-    endpoint: 'http://localhost:3009/graphql',
-    settings: {
-      'editor.theme': 'light',
-    },
-  },
-});
+// const server = new ApolloServer({
+//   schema,
+//   context: async () => {
+//     const context = {
+//       loaders: genLoaders(),
+//     };
+//     return context;
+//   },
+//   introspection: true,
+//   playground: {
+//     endpoint: 'http://localhost:3009/graphql',
+//     settings: {
+//       'editor.theme': 'light',
+//     },
+//   },
+// });
 
-module.exports = server;
+// module.exports = server;
